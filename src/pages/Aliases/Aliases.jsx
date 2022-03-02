9a2e71367de95efe06c9a20efd4fa8531da7d06b
Jeff_Gore <JefGore@gmail.com>
2022-03-02 17:19:45 -0500

    style alias page

--- src/pages/Aliases/Aliases.jsx
+++ src/pages/Aliases/Aliases.jsx
@@ -6,33 +6,41 @@ import { FaHeart } from 'react-icons/fa'
 const Aliases = () => {
 
   const [currentAlias, setCurrentAlias] = useState(0)
-
+  const [showHeart, setShowHeart] = useState(false)
+  const [showInstructions, setshowInstructions] = useState(true)
+  
+  const transition = () => {
+    setShowHeart(false)
+    setshowInstructions(false)
+    setCurrentAlias(0)
+    setTimeout(() => {  setShowHeart(true); setCurrentAlias(AliasList[Math.floor(Math.random() * AliasList.length)])}, 200)
+  }
 
   return (
     <div className={`${styles.aliasesContainer}`}>
       <h1 className='pageTitle'>Aliases</h1>
-        <button
-          onClick={() =>{
-            setCurrentAlias(AliasList[Math.floor(Math.random() * AliasList.length)])
-          }}
-        >Click</button>
       <div className={`${styles.eightBallContainer}`}>
         <div className={`${styles.eightBallOutter}`}
           onClick={() =>{
-            setCurrentAlias(AliasList[Math.floor(Math.random() * AliasList.length)])
+            transition()
           }}
         >
           <div className={`${styles.eightBallRing}`}>
             <div className={`${styles.eightBallInner}`}>
               <div className={`${styles.heartContainer}`}>
-                {currentAlias !== 0 ? 
-                <h5 className={`${styles.alias}`}>{`${currentAlias.name}`}</h5> : <h5>Click to see a nickname</h5>}
-                {currentAlias !== 0 && <FaHeart className={`${styles.heart}`}></FaHeart>}
+                {showInstructions && <h5 className={`${styles.aliasInstruct}`}>Click to see a nickname</h5>}
+                {currentAlias !== 0 && <h5 className={`${styles.alias}`}>{`${currentAlias.name}`}</h5> }
+                {showHeart && <FaHeart className={`${styles.heart}`} ></FaHeart>}
               </div>
             </div>
           </div>
         </div>
       </div>
+      <button
+          onClick={() =>{
+            transition()
+          }}
+        >Click to see a nickname</button>
     </div>
   )
 }
