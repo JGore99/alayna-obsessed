c2dd26542987b5292b7a3436f7c0e9d65d5f9388
Jeff_Gore <JefGore@gmail.com>
2022-03-17 13:10:06 -0400

    eliminate duplicates in random alias display, and add counter

--- src/pages/Aliases/Aliases.jsx
+++ src/pages/Aliases/Aliases.jsx
@@ -5,32 +5,45 @@ import { FaHeart } from 'react-icons/fa'
 
 const Aliases = () => {
 
-  const [currentAlias, setCurrentAlias] = useState(0)
+  const [currentAlias, setCurrentAlias] = useState()
+  const [remainingAliases, setremainingAliases] = useState([...AliasList])
   const [showHeart, setShowHeart] = useState(false)
   const [showInstructions, setshowInstructions] = useState(true)
+  const [disabled, setDisabled] = useState()
   
-  const transition = () => {
+  const showAlias = () => {
+    setCurrentAlias(0)
     setShowHeart(false)
     setshowInstructions(false)
-    setCurrentAlias(0)
-    setTimeout(() => {  setShowHeart(true); setCurrentAlias(AliasList[Math.floor(Math.random() * AliasList.length)])}, 200)
+    let newCurrentAlias = remainingAliases[Math.floor(Math.random() * remainingAliases.length)]
+    setCurrentAlias(newCurrentAlias)
+    let updatedAliases = remainingAliases.filter(remainingAlias => remainingAlias.id !== newCurrentAlias.id)
+    setremainingAliases(updatedAliases)
+    console.log("remainingAliases", remainingAliases)
+    console.log("length", remainingAliases.length)
+    setTimeout(() => {
+      setShowHeart(true)}, 400)
+    setDisabled(remainingAliases.length === 1 ? true : false)
   }
 
   return (
     <div className={`${styles.aliasesContainer}`}>
       <h1 className='pageTitle'>Aliases</h1>
-      <h3>{AliasList.length} and counting</h3>
+      <h3>{remainingAliases.length} remaing</h3>
+      {disabled && <h3>That's all of them, for now.</h3>}
       <div className={`${styles.eightBallContainer}`}>
-        <div className={`${styles.eightBallOutter}`}
+      <button className={`${styles.eightBallButton}`}
+          disabled={disabled}
           onClick={() =>{
-            transition()
+            showAlias()
           }}
-        >
+      ></button>
+        <div className={`${styles.eightBallOutter}`}>
           <div className={`${styles.eightBallRing}`}>
             <div className={`${styles.eightBallInner}`}>
               <div className={`${styles.heartContainer}`}>
                 {showInstructions && <h5 className={`${styles.aliasInstruct}`}>Click to see a nickname</h5>}
-                {currentAlias !== 0 && <h5 className={`${styles.alias}`}>{`${currentAlias.name}`}</h5> }
+                {showHeart && <h5 className={`${styles.alias}`}>{`${currentAlias.name}`}</h5> }
                 {showHeart && <FaHeart className={`${styles.heart}`} ></FaHeart>}
               </div>
             </div>
@@ -38,8 +51,9 @@ const Aliases = () => {
         </div>
       </div>
       <button
-          onClick={() =>{
-            transition()
+        disabled={disabled}
+        onClick={() =>{
+          showAlias()
           }}
         >Click</button>
     </div>
